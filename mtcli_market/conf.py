cf85381fd6d125d4d2c55a7f1b3c16b04032a31c
Valmir França <vfranca3@gmail.com>
2025-11-23 07:56:16 -0300

    Revisando os calculos

--- mtcli_market/conf.py
+++ mtcli_market/conf.py
@@ -7,7 +7,6 @@ PERIOD = os.getenv("PERIOD", config["DEFAULT"].get("period", fallback="M1"))
 PERIODOS = int(
     os.getenv("PERIODOS", str(config["DEFAULT"].getint("periodos", fallback=566)))
 )
-# Allow fractional block sizes; store as float
 BLOCK = float(os.getenv("BLOCK", str(config["DEFAULT"].get("block", fallback="100"))))
 BY = os.getenv("BY", config["DEFAULT"].get("by", fallback="time"))
 IB = int(os.getenv("IB", str(config["DEFAULT"].getint("ib", fallback=30))))
